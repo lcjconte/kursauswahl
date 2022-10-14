69a9bd03d6442523579ad63a470367f3b7d42905
Leandro Conte <lcjconte@gmail.com>
2022-10-14 17:44:38 +0200

    Changes to db_manage

--- src/db_manage.js
+++ src/db_manage.js
@@ -1,6 +1,4 @@
-const { Router } = require("express");
 var db = require("../src/db");
-
 
 async function register(rgsusername, rgsisadmin, rgspwd, rgssalt) {
     var result = await db.query(`INSERT INTO userdata (username, isadmin, pwd, salt) VALUES ($1, $2, $3, $4);`, [rgsusername, rgsisadmin, rgspwd, rgssalt])
@@ -10,13 +8,12 @@ async function register(rgsusername, rgsisadmin, rgspwd, rgssalt) {
     else return true;
 }
 async function update_pwd(userid, newpwd, newsalt) {
-    var result = await db.query(`UPDATE userdata SET pwd = $2, salt =$3 WHERE userid = $1`, [userid, newpwd, newsalt])
+    var result = await db.query(`UPDATE userdata SET pwd = $2, salt = $3 WHERE userid = $1`, [userid, newpwd, newsalt])
     if (result.rowCount === 0) {
         return false;
     }
     return true;
 }
-
 async function update_isadmin(userid, isadmin) {
     var result = await db.query(`UPDATE userdata SET isadmin = $1 WHERE userid = $2`, [isadmin, userid])
     if (result.rowCount === 0) {
@@ -25,17 +22,45 @@ async function update_isadmin(userid, isadmin) {
     return true;
 }
 async function delete_user(userid) {
-    var result = await db.query(`DELETE FROM userdata WHERE userid = $1`, [userid])
-    if (result.rowCount === 0) {
-        return false;
+    let ans = false
+    try {
+        const client = await db.connect()
+        try {
+            await client.query('BEGIN')
+            await client.query('DELETE FROM selections WHERE userid = $1', [userid])
+            await client.query("DELETE FROM userdata WHERE userid = $1", [userid])
+            await client.query('COMMIT')
+            ans = true
+        } catch (e) {
+            await client.query('ROLLBACK')
+            throw e
+        } finally {
+            client.release()
+        }
+    } catch (error) {
+        console.log(error.stack)
     }
+    return ans
+
 }
 async function get_selection(userid) {
-    var result = await db.query(`SELECT * FROM selections WHERE userid = $1`, [userid])
+    var result = (await db.query(`SELECT * FROM selections WHERE userid = $1`, [userid]))[0];
     return result;
 }
+async function set_selection_alt(userid, sel) {
+    let f = s => sel[s]!==undefined;
+    return set_selection(userid, 
+        f("de"), f("ita"), f("en"), f("fr"), f("lat"), f("mu"), f("ku"), 
+        f("ges"), f("eco"), f("git"), f("biphi"), f("etk"), f("rel"), f("geo"),
+        f("bio"), f("phy"), f("ma"), f("ch"), f("inf"), f("sport"), f("soz"));
+}
 async function set_selection(userid, de, ita, en, fr, lat, mu, ku, ges, eco, git, biphi, etk, rel, geo, bio, phy, ma, ch, inf, sport, soz) {
-    var result = await db.query(`insert into selections values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22)`, [userid, de, ita, en, fr, lat, mu, ku, ges, eco, git, biphi, etk, rel, geo, bio, phy, ma, ch, inf, sport, soz])
+    var result = await db.query("UPDATE selections SET de=$1, ita=$2, en=$3, fr=$4, lat=$5, "+ 
+                                "mu=$6, ku=$7, ges=$8, eco=$9, git=$10, biphi=$11, etk=$12, "+
+                                "rel=$13, geo=$14, bio=$15, phy=$16, ma=$17, ch=$18, inf=$19, sport=$20, soz=$21 "+
+                                "WHERE userid=$22", 
+                                [de, ita, en, fr, lat, mu, ku, ges, eco, git, biphi, etk, 
+                                    rel, geo, bio, phy, ma, ch, inf, sport, soz, userid])
     if (result.rowCount === 0) {
         return false;
     }
@@ -55,4 +80,5 @@ async function user_by_userid(userid) {
     }
     return result.rows[0];
 }
-module.exports = { register, update_pwd, update_isadmin, delete_user, get_selection, set_selection, register, user_by_name, user_by_userid };
+
+module.exports = { register, update_pwd, update_isadmin, delete_user, get_selection, set_selection, set_selection_alt, register, user_by_name, user_by_userid};
